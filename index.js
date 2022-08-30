2d99f134dba7ab27dae472655233e82016c3881e
leewellmacharia10@gmail.com <leewellmacharia10@gmail.com>
2022-08-30 10:47:26 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
+  let reversedWord = word.split("").reverse("").join("");
+  if (reversedWord === word) return true;
+
+  return false
+
+  console.log(isPand)
+
   // Write your algorithm here
 }
 
 /* 
   Add your pseudocode here
+  declare a function
+  make the function return true or false if a value is passed to check if it's a palindrome or not
 */
 
 /*
   Add written explanation of your solution here
+  My function should be able to pass words and if read either from the first letter to the last or the last to the first, they are the same. a "tot" can be read tot mean the same thing whether read from the start or from the end.
 */
 
 // You can run `node index.js` to view these console logs
